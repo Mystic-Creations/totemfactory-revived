13d10cebe421f3c87e84202d4d220155ca45ece8
JustMili <rodx160@gmail.com>
2025-06-04 16:52:56 +0200

    Initial

---
+++ src/main/java/net/justmili/totemfactory/item/IncompleteTotemBodyCasing.java
@@ -0,0 +1,31 @@
+package net.justmili.totemfactory.item;
+
+import net.minecraft.world.level.Level;
+import net.minecraft.world.item.TooltipFlag;
+import net.minecraft.world.item.Rarity;
+import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.item.Item;
+import net.minecraft.network.chat.Component;
+
+import net.justmili.totemfactory.init.TotemFactoryTabs;
+
+import net.fabricmc.fabric.api.itemgroup.v1.ItemGroupEvents;
+
+import java.util.List;
+
+public class IncompleteTotemBodyCasing extends Item {
+	public IncompleteTotemBodyCasing() {
+		super(new Item.Properties().stacksTo(1).rarity(Rarity.UNCOMMON));
+		ItemGroupEvents.modifyEntriesEvent(TotemFactoryTabs.TAB_TOTEM_FACTORY).register(content -> content.accept(this));
+	}
+
+	@Override
+	public int getUseDuration(ItemStack itemstack) {
+		return 0;
+	}
+
+	@Override
+	public void appendHoverText(ItemStack itemstack, Level world, List<Component> list, TooltipFlag flag) {
+		super.appendHoverText(itemstack, world, list, flag);
+	}
+}
