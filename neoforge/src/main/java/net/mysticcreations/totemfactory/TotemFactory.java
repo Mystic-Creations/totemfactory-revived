dc870ae61e6e35b1ef0e00c3a29d02f008a37547
JustMili <rodx160@gmail.com>
2025-09-16 22:20:44 +0200

    neoforge is a pain in the ass and idk what I'm doing anymore

--- neoforge/src/main/java/net/mysticcreations/totemfactory/TotemFactory.java
+++ neoforge/src/main/java/net/mysticcreations/totemfactory/TotemFactory.java
@@ -4,7 +4,6 @@ import net.mysticcreations.totemfactory.init.TfItems;
 import net.mysticcreations.totemfactory.init.TfTabs;
 import net.neoforged.bus.api.IEventBus;
 import net.neoforged.fml.common.Mod;
-import net.neoforged.fml.javafmlmod.FMLJavaModLoadingContext;
 import org.apache.logging.log4j.Logger;
 import org.apache.logging.log4j.LogManager;
 
@@ -13,11 +12,11 @@ public class TotemFactory {
 	public static final Logger LOGGER = LogManager.getLogger(TotemFactory.class);
 	public static final String MODID = "totemfactory";
 
-	public TotemFactory(FMLJavaModLoadingContext modContext) {
-		MinecraftForge.EVENT_BUS.register(this);
-		IEventBus EVENT_BUS = modContext.getModEventBus();
+	public TotemFactory(IEventBus modBus) {
+		NeoForge
 
-		TfItems.REGISTER.register(EVENT_BUS);
-		TfTabs.REGISTER.register(EVENT_BUS);
+		// register DeferredRegister objects to the mod's event bus
+		TfItems.REGISTER.register(modEventBus);
+		TfTabs.REGISTER.register(modEventBus);
 	}
 }
