f20962ca1145ba0bc968f2653dfe1b9e59b92f7b
JustMili <rodx160@gmail.com>
2025-09-17 21:51:47 +0200

    Update ✨

--- forge/src/main/java/net/mysticcreations/totemfactory/TotemFactory.java
+++ forge/src/main/java/net/mysticcreations/totemfactory/TotemFactory.java
@@ -1,10 +1,10 @@
 package net.mysticcreations.totemfactory;
 
+import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
 import net.mysticcreations.totemfactory.init.TfItems;
 import net.mysticcreations.totemfactory.init.TfTabs;
 import org.apache.logging.log4j.Logger;
 import org.apache.logging.log4j.LogManager;
-import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.eventbus.api.IEventBus;
 import net.minecraftforge.common.MinecraftForge;
@@ -14,11 +14,11 @@ public class TotemFactory {
 	public static final Logger LOGGER = LogManager.getLogger(TotemFactory.class);
 	public static final String MODID = "totemfactory";
 
-	public TotemFactory(FMLJavaModLoadingContext modContext) {
+	public TotemFactory() {
 		MinecraftForge.EVENT_BUS.register(this);
-		IEventBus EVENT_BUS = modContext.getModEventBus();
+		IEventBus modEventBus = FMLJavaModLoadingContext.get().getModEventBus();
 
-		TfItems.REGISTER.register(EVENT_BUS);
-		TfTabs.REGISTER.register(EVENT_BUS);
+		TfItems.REGISTER.register(modEventBus);
+		TfTabs.REGISTER.register(modEventBus);
 	}
 }
